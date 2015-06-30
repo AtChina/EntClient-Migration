d73d4a5ab72d70e0ba9acc730cabf270f0ae2b2f
AtChina <weichangying@msn.com>
2015-06-30 17:15:20 +0800

    ADD

--- _sqls/sql.clear_others.sql
+++
@@ -1,8 +0,0 @@
---清除模块配置垃圾数据
-DELETE FROM xwmodfunction  WHERE XWMODCHILDID IN(SELECT XWMODCHILDID FROM xwmodularchild WHERE xwmodid IN(SELECT XWMODID FROM xwmodular where xwenterprisenumber <>1002568))
-DELETE FROM xwmodcondition  WHERE XWMODCHILDID IN(SELECT XWMODCHILDID FROM xwmodularchild WHERE xwmodid IN(SELECT XWMODID FROM xwmodular where xwenterprisenumber<>1002568))
-DELETE FROM xwmodularchild WHERE xwmodid IN(SELECT XWMODID FROM xwmodular where xwenterprisenumber <> 1002568)
-DELETE FROM xwmodular where xwenterprisenumber <>1002568
---清除本地表垃圾数据
-DELETE FROM xwlocalfield where xwdatasourceid in(SELECT xwdatasourceid FROM xwlocaldatasource WHERE xwenterprisenumber <>1002568)
-DELETE FROM xwlocaldatasource WHERE xwenterprisenumber <>1002568
